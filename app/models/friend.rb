9de038ad2eb3e36a87e4ef42d73924d0ce3fc5f4
Eric McKee <lyolayale19@gmail.com>
2023-12-30 21:24:15 -0600

    Created validations/rspec -> TDD.

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,6 @@
 class Friend < ApplicationRecord
-  validates :name, :event, :balance, presence: true
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :event, presence: true, length: { minimum: 5 }
+  validates :balance, presence: true
+  validates_numericality_of :balance, only_numeric: true, allow_nil: true
 end
